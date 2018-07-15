d3af43becd98364bf98c7b461898acd126837da5
luciferL <luciferlikeme@163.com>
2018-07-15 23:03:58 +0800

    hello

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,84 +1,13 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <p>{{title}}</p>
+    <p v-if="show">{{subTitle}}</p>
+    <div>
+      <input type="text" v-model="todo">
+      <button @click="handleClick">添加</button>
+    </div>
     <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
+      <li v-for="lists in list" :key='lists'>{{lists}}</li>
     </ul>
   </div>
 </template>
@@ -88,7 +17,18 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      title: 'hello',
+      subTitle: '看见我',
+      show: true,
+      todo: '',
+      list: ['吃饭', '睡觉', '敲代码']
+    }
+  },
+  methods: {
+    handleClick () {
+      // this.title = '你好 小程序'
+      this.list.push(this.todo)
+      this.todo = ''
     }
   }
 }
@@ -96,7 +36,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
